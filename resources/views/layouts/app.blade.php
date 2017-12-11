eb837a06c0a974d1af577c48dd0782eb1d2e2184
Cyadenz <ugo.perniceni@hotmail.fr>
2017-12-11 16:34:07 +0100

    Ajout Tables&Config site

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Parking') }}</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -29,7 +29,7 @@
 
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
+                        {{ config('app.name', 'Parking') }}
                     </a>
                 </div>
 
@@ -43,11 +43,13 @@
                     <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
                         @guest
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
+                            <li><a href="{{ route('login') }}">Se connecter</a></li>
+                            <li><a href="{{ route('register') }}">S'enregistrer</a></li>
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
+				                    @if(\Auth::user()->admin == 1){{'Administrateur'}}
+				                    @endif
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
@@ -56,7 +58,7 @@
                                         <a href="{{ route('logout') }}"
                                             onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                            Logout
+                                            Déconnexion
                                         </a>
 
                                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,11 +83,13 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    @auth
+                    <a href="https://laravel.com/docs">Reservation</a>
+                    @if(\Auth::user()->admin == 1)
+                    <a href="https://laracasts.com">Administration</a>
+                    @endif
+                    <a href="https://laracasts.com">Mon compte</a>
+                    @endauth
                 </div>
             </div>
         </div>
